e05f3ce14c46dbc4a32122d621412dc169c2256f
Tran Quang Nhut <ntran64634@gmail.com>
2026-02-23 12:07:16 +0700

    bosunganh

--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Gamer', 'Trai Dep Binh Tan'],
+    strings: ['Học Giỏi', 'Tinh Tế', 'Đẹp Trai'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
